f45c7c8e11508ab51f75e6fa9f9ffe01f813553c
Megumin <1245808560@qq.com>
2022-07-18 23:50:43 +0800

    * 做完了lua练习题

--- Execrise2.lua
+++ Execrise2.lua
@@ -1,32 +1,46 @@
 Timer = {
     tick = 0,
-    countDownList = {}
+    countDown = {}
 }
 
 function Timer:update()
     self.tick = self.tick + 1
-    -- 遍历倒计时表
-    for i = #self.countDownList, 1, -1 do
-        local l_countDown = self.countDownList[i]
-        l_countDown.tempTick = l_countDown.tempTick + 1
-        if l_countDown.tempTick == l_countDown.time then
-            l_countDown:fun()
-            if l_countDown.canRepetition then
-                l_countDown.tempTick = 0
-            else
-                table.remove(self.countDownList,i)
-            end
+    if self.countDown == nil then
+        return
+    end
+    self.countDown.tempTick =  self.countDown.tempTick + 1
+    if self.countDown.tempTick == self.countDown.time then
+        self.countDown:fun()
+        if self.countDown.canRepetition then
+            self.countDown.tempTick = 0
+        else
+            self.countDown = nil
         end
     end
 end
+local handleTimeStr = function (time)
+    local l_str = ""
+    if(time % 10 == time) then
+        l_str = "0".. tostring(time)
+    else
+        l_str = tostring(time)
+    end
+    return l_str
+end
 
 function  Timer:showTime()
+    
     local l_second = self.tick % 60
     local l_min =  (self.tick / 60) % 60
-    l_min = math.modf(l_min)
+    l_min = math.floor(l_min)
     local l_hour = (l_min / 60) % 60
-    l_hour = math.modf(l_hour)
-    print(string.format("%d:%d:%d", l_hour, l_min, l_second))
+    l_hour = math.floor(l_hour)
+
+    local l_sStr  = handleTimeStr(l_second)
+    local l_mStr = handleTimeStr(l_min)
+    local l_hStr = handleTimeStr(l_hour)
+    local l_timeStr = l_hStr .. ":" ..l_mStr .. ":" .. l_sStr
+    print(l_timeStr)
 end
 
 function Timer:setTimer(time,fun,canRepetition)
@@ -43,16 +57,16 @@ function Timer:setTimer(time,fun,canRepetition)
         fun = fun,
         canRepetition = canRepetition,
     }
-    table.insert(self.countDownList,l_cuntDown)
+    self.countDown = l_cuntDown
 end
 
-function Timer:new()
-    local o
-    o = o or {}
-    setmetatable(o,self)
-    self.__index = self
-    return o
-end
+-- function Timer:new()
+--     local o
+--     o = o or {}
+--     setmetatable(o,self)
+--     self.__index = self
+--     return o
+-- end
 
 local Building = {
     lv = "",
@@ -61,13 +75,13 @@ local Building = {
     getNum = 0,
 }
 
-function Building:new()
-    local o
-    o = o or {}
-    setmetatable(o,self)
-    self.__index = self
-    return o
-end
+-- function Building:new()
+--     local o
+--     o = o or {}
+--     setmetatable(o,self)
+--     self.__index = self
+--     return o
+-- end
 
 function CreateClass(...)
     local c = {}
@@ -82,7 +96,6 @@ function CreateClass(...)
     end})
     c.__index = c
     function c:New(o)
-        local o
         o = o or {}
         setmetatable(o, c)
         return o
@@ -91,35 +104,37 @@ function CreateClass(...)
   end
 
 local main = function ()
-    local farm = CreateClass(Timer:new(),Building:new())
-    local militaryCamp = CreateClass(Timer:new(),Building:new())
-    farm.lv = 1
+    local farm = CreateClass(Timer,Building)
+    local militaryCamp = CreateClass(Timer,Building)
+    farm.lv = 10
     farm.name = "农场"
     farm.product = "粮食"
 
     militaryCamp.lv = 1
-    militaryCamp.name = "兵营"
-    militaryCamp.product = "兵力"
+    militaryCamp.name = "军营"
+    militaryCamp.product = "士兵"
 
     farm:setTimer(15,function ()
         farm.getNum = 100 * farm.lv + farm.getNum
         if(farm.getNum == 1000) then
             farm:showTime()
             print(string.format("%s收获%d %s",farm.name,farm.getNum,farm.product))
+            farm.getNum = 0
         end
     end,true)
 
     militaryCamp:setTimer(20,function ()
         militaryCamp.getNum = 2 * militaryCamp.lv + militaryCamp.getNum
-        farm:showTime()
+        militaryCamp:showTime()
         print(string.format("%s收获%d %s",militaryCamp.name,militaryCamp.getNum,militaryCamp.product))
     end,true)
 
-    -- for i = 1 , 120 do
-    --     militaryCamp:update()
-    --     farm:update()
-    -- end
+    for i = 1 , 120 do
+        militaryCamp:update()
+        farm:update()
+    end
 end
 
-main()
+ main()
+
 
